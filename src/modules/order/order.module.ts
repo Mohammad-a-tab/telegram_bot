28f7016edfc7c6e5663a6474c1c2f3836777fc9b
Mohammad Aali <mohammad.aali@g2holding.org>
2026-05-12 19:48:47 +0330

    chore: enhanced code base.

--- src/modules/order/order.module.ts
+++ src/modules/order/order.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Order } from './entities/order.entity';
+import { OrderHandler } from '../bot/handlers/order.handler';
 import { PendingOrderCheckerService } from './pending-order.checker.service';
+import { CacheModule } from '../cache/cache.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order])],
-  controllers: [],
-  providers: [PendingOrderCheckerService],
-  exports: [PendingOrderCheckerService, TypeOrmModule],
+  imports: [
+    TypeOrmModule.forFeature([Order]),
+    CacheModule,
+  ],
+  providers: [OrderHandler, PendingOrderCheckerService],
+  exports: [OrderHandler, PendingOrderCheckerService, TypeOrmModule],
 })
 export class OrderModule {}
